067fc237bebfe19f98e3aaba262facf1cc768fb3
Thai Duc Dung <dungtd10@fpt.com.vn>
2020-11-05 23:56:02 +0700

    add color to baraction add volume indicator

--- baraction.sh
+++ baraction.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+print_vol() {
+  VOL_STATUS=$1
+  VOL_LEVEL=$2
+  if [ "$VOL_STATUS" = "on" ];
+  then
+    echo "+@fg=1;Vol: $2"
+  else
+    echo "+@fg=2;Vol: $2"
+  fi
+}
+
 print_batt() {
   CHARGE_STATUS="$4"
   PERCENT=$(echo $5 | sed 's/\..*/%/')
@@ -7,40 +18,42 @@ print_batt() {
 
   case $CHARGE_STATUS in
     Charging,)
-      echo -n "⊣ Bat°: $PERCENT [$TIME_LEFT] ⊢"
+      echo -n "+@fg=1;Bat: $PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;]"
       ;;
     Discharging,)
-      echo -n "⊣ Bat: $PERCENT [$TIME_LEFT] ⊢"
+      echo -n "+@fg=2;Bat: $PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;]" 
       ;;
     Full,)
-      echo -n "⊣ Bat°: $PERCENT ⊢"
+      echo -n "+@fg=1;Bat: $PERCENT"
       ;;
-    # *)
-    #   echo -n "⊣ No bat ⊢"
-    #   ;;
   esac
 }
 
 print_cpu() {
   PERCENT_CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' | cut -d '.' -f 1)
-  echo -n "⊣ CPU: ${PERCENT_CPU}% ⊢"
+  echo -n "+@fg=1;CPU: ${PERCENT_CPU}%+@fg=0;"
 }
 
 print_uptime() {
   UP=$(uptime | awk '{print $3, $4}' | sed 's/,//' | sed 's/\s[0-9]*//')
-  echo -n "⊣ Up: ${UP} ⊢"
+  echo -n "+@fg=1;Up: ${UP}+@fg=0;"
 }
 
 print_mem() {
   MEM=`/usr/bin/free -m | grep ^Mem | awk '{printf "%i", $3 / $2 * 100}'`
-  echo -n "⊣ RAM: ${MEM}% ⊢"
+  echo -n "+@fg=1;RAM: ${MEM}%+@fg=0;"
 }
 
+SLEEP_SEC=3
 while :; do
-  print_mem &
-  print_cpu &
-  print_batt `acpitool` &
-  print_uptime &
-  echo ""
-  sleep 300
+  VOL_CMD=`amixer get Master | awk -F'[][]' 'END{ print $6, $2 }'`
+
+  echo "+@fg=0;⊣ $(print_cpu &) +@fg=0;⊢⊣ \
+$(print_mem &) +@fg=0;⊢⊣ \
+$(print_uptime &) +@fg=0;⊢⊣ \
+$(print_batt `acpitool` &) +@fg=0;⊢⊣ \
+$(print_vol $VOL_CMD &)+@fg=0; ⊢"
+
+	sleep $SLEEP_SEC
 done
+
