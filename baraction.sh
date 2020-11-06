a7eff50aee9808b3d481b192ff99359fa30b9087
DungTD10 <mducdung255@gmail.com>
2020-11-06 08:19:52 +0700

    add color to status bar

--- baraction.sh
+++ baraction.sh
@@ -3,12 +3,14 @@
 print_vol() {
   VOL_STATUS=$1
   VOL_LEVEL=$2
-  if [ "$VOL_STATUS" = "on" ];
-  then
-    echo "+@fg=1;Vol: +@fg=1;$2"
-  else
-    echo "+@fg=2;Vol: +@fg=1;$2"
-  fi
+  case $VOL_STATUS in
+    on)
+      echo "+@fg=0;⊣ +@fg=3;Vol: +@fg=1;$2 +@fg=0;⊢"
+      ;;
+    off)
+      echo "+@fg=0;⊣ +@fg=2;Vol: +@fg=1;$2 +@fg=0;⊢"
+      ;;
+  esac
 }
 
 print_batt() {
@@ -18,41 +20,41 @@ print_batt() {
 
   case $CHARGE_STATUS in
     Charging,)
-      echo -n "+@fg=1;Bat: +@fg=1;$PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;]"
+      echo -n "+@fg=0;⊣ +@fg=3;Bat: +@fg=1;$PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;] +@fg=0;⊢"
       ;;
     Discharging,)
-      echo -n "+@fg=2;Bat: +@fg=1;$PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;]" 
+      echo -n "+@fg=0;⊣ +@fg=2;Bat: +@fg=1;$PERCENT +@fg=0;[+@fg=1;$TIME_LEFT+@fg=0;] +@fg=0;⊢" 
       ;;
     Full,)
-      echo -n "+@fg=1;Bat: $PERCENT"
+      echo -n "+@fg=0;⊣ +@fg=3;Bat: $PERCENT +@fg=0;⊢"
       ;;
   esac
 }
 
 print_cpu() {
   PERCENT_CPU=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' | cut -d '.' -f 1)
-  echo -n "+@fg=1;CPU: ${PERCENT_CPU}%+@fg=0;"
+  echo -n "+@fg=0;⊣ +@fg=3;CPU: ${PERCENT_CPU}%+@fg=0; +@fg=0;⊢"
 }
 
 print_uptime() {
   UP=$(uptime | awk '{print $3, $4}' | sed 's/,//' | sed 's/\s[0-9]*//')
-  echo -n "+@fg=1;Up: ${UP}+@fg=0;"
+  echo -n "+@fg=0;⊣ +@fg=1;Up: ${UP}+@fg=0; +@fg=0;⊢"
 }
 
 print_mem() {
   MEM=`/usr/bin/free -m | grep ^Mem | awk '{printf "%i", $3 / $2 * 100}'`
-  echo -n "+@fg=1;RAM: ${MEM}%+@fg=0;"
+  echo -n "+@fg=0;⊣ +@fg=1;RAM: ${MEM}%+@fg=0; +@fg=0;⊢"
 }
 
 SLEEP_SEC=5
 while :; do
   VOL_CMD=`amixer get Master | awk -F'[][]' 'END{ print $6, $2 }'`
 
-  echo "+@fg=0;⊣ $(print_cpu &) +@fg=0;⊢⊣ \
-$(print_mem &) +@fg=0;⊢⊣ \
-$(print_uptime &) +@fg=0;⊢⊣ \
-$(print_batt `acpitool` &) +@fg=0;⊢⊣ \
-$(print_vol $VOL_CMD &)+@fg=0; ⊢"
+  echo "$(print_cpu &)\
+$(print_mem &)\
+$(print_batt `acpitool` &)\
+$(print_uptime &)\
+$(print_vol $VOL_CMD &)"
 
 	sleep $SLEEP_SEC
 done
